0bc0999295cadf766f0eeb8035ad8e4b505e2c20
Clay Sedgwick <89625299+claysedgwick@users.noreply.github.com>
2021-10-06 18:10:03 -1000

    lab8

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,15 +1,33 @@
 require("./products_data.js");
 
-var num_products = 5;
-var count = 1;
+for (var count = 1; eval("typeof name"+count) != 'undefined'; count++) {
+    console.log(`${count}. ${eval('name' + count)}`)
+}
 
-while (count <= num_products) {
+/*
+var num_products = 5;
+for (var count = 1; eval("typeof name"+count) != 'undefined'; count++) {
     if (count > num_products/2) {
         console.log("That's enough!");
-        break;
+        process.exit();
     }
     console.log(`${count}. ${eval('name' + count)}`);
-    count++;
 }
+*/ 
+/*
+var num_products = 5;
+var count = 0;
+
+while (count < num_products) {
+    count++;
+    if (count > num_products/2) {
+        console.log("Don't ask for anything else.");
+        process.exit();
+    }
+    else {
+        console.log(`${count}. ${eval('name' + count)}`);
+    }
+}
+*/
 
 console.log("That's all we have!");
