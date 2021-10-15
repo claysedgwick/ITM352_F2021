fc01b7e2f6584b0a02c5c8d84c374e05d8578991
Clay Sedgwick <clayds@hawaii.edu>
2021-10-15 00:41:54 -1000

    wodspp3

---
+++ WODs/SmartPhoneProducts3/products_data.js
@@ -0,0 +1,27 @@
+
+products = [
+products1 =
+{"name" : "HTC",
+"price" : 40.00,
+"image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"},
+
+products2 =
+{"name" : "Apple",
+"price" : 75.00,
+"image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"},
+
+products3 =
+{"name" : "Nokia",
+"price" : 35.00,
+"image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"},
+
+products4 =
+{"name" : "Samsung",
+"price" : 45.00,
+"image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"},
+
+products5 =
+{"name" : "Blackberry",
+"price" : 10.00,
+"image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"}
+]
